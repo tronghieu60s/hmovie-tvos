6f78889c52dd2490492383faa33caa04fcc56390
Trong Hieu <tronghieu60s@gmail.com>
2024-06-25 22:05:57 +0700

    firebase firestore

--- metro.config.js
+++ metro.config.js
@@ -10,7 +10,7 @@ const config = getDefaultConfig(__dirname); // eslint-disable-line no-undef
 //
 // Metro will still resolve source files with standard extensions
 // as usual if TV-specific files are not found for a module.
-//
+
 if (process.env?.EXPO_TV === "1") {
   const originalSourceExts = config.resolver.sourceExts;
   const tvSourceExts = [
@@ -19,5 +19,7 @@ if (process.env?.EXPO_TV === "1") {
   ];
   config.resolver.sourceExts = tvSourceExts;
 }
+
+config.resolver.sourceExts.push("cjs");
 
 module.exports = config;
